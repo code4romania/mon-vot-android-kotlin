c806f51e3872a314640701f6cedfc0138af58902
Daniel <daniel.berbece97@gmail.com>
2019-10-10 16:17:15 +0300

    Merge with upstream

--- app/src/main/java/ro/code4/monitorizarevot/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/ro/code4/monitorizarevot/ui/splashscreen/SplashScreenActivity.kt
@@ -3,16 +3,15 @@ package ro.code4.monitorizarevot.ui.splashscreen
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import ro.code4.monitorizarevot.R
-import ro.code4.monitorizarevot.helper.getToken
 import ro.code4.monitorizarevot.ui.base.BaseActivity
 import ro.code4.monitorizarevot.ui.login.LoginActivity
 import ro.code4.monitorizarevot.ui.main.MainActivity
 
 class SplashScreenActivity: BaseActivity<SplashScreenViewModel>() {
-    private val sharedPreferences: SharedPreferences by inject()
 
     override val layout: Int
         get() = R.layout.activity_splash_screen
@@ -20,16 +19,18 @@ class SplashScreenActivity: BaseActivity<SplashScreenViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent: Intent
 
-        // Check if user is logged in or not
-        if (sharedPreferences.getToken() != null) {
-            intent = Intent(this, MainActivity::class.java)
-        } else {
-            intent = Intent(this, LoginActivity::class.java)
+        val tokenObserver = Observer<String?> { tokenValue ->
+            val intent: Intent
+            when (tokenValue) {
+                null -> intent = Intent(this, MainActivity::class.java)
+                else -> intent = Intent(this, LoginActivity::class.java)
+            }
+
+            startActivity(intent)
+            finish()
         }
 
-        startActivity(intent)
-        finish()
+        viewModel.tokenLiveData.observe(this, tokenObserver)
     }
 }
--- app/src/main/java/ro/code4/monitorizarevot/ui/splashscreen/SplashScreenViewModel.kt
+++ app/src/main/java/ro/code4/monitorizarevot/ui/splashscreen/SplashScreenViewModel.kt
@@ -1,5 +1,20 @@
 package ro.code4.monitorizarevot.ui.splashscreen
 
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
+import org.koin.core.inject
+import ro.code4.monitorizarevot.helper.getToken
 import ro.code4.monitorizarevot.ui.base.BaseViewModel
 
-class SplashScreenViewModel: BaseViewModel()
+class SplashScreenViewModel: BaseViewModel() {
+    val tokenLiveData: MutableLiveData<String?> by lazy {
+        MutableLiveData<String?>()
+    }
+
+    private val sharedPreferences: SharedPreferences by inject()
+
+    init {
+        tokenLiveData.value = sharedPreferences.getToken()
+    }
+
+}
