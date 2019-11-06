00d22bdd7cc1d327fe2dc2684d603829eba1e4a4
Alexandra Daraban <alexandra.daraban@qualitance.com>
2019-11-06 14:02:06 +0200

    set locale  (for api <24)

--- app/src/main/java/ro/code4/monitorizarevot/helper/LocaleManager.kt
+++ app/src/main/java/ro/code4/monitorizarevot/helper/LocaleManager.kt
@@ -7,19 +7,17 @@ import java.util.*
 
 object LocaleManager {
 
+    @Suppress("DEPRECATION")
     fun wrapContext(context: Context): Context {
 
         val savedLocale =
             PreferenceManager.getDefaultSharedPreferences(context).getLocaleCode().getLocale()
-
-
         // as part of creating a new context that contains the new locale we also need to override the default locale.
         Locale.setDefault(savedLocale)
 
-        // create new configuration with the saved locale
-        val newConfig = Configuration()
-        newConfig.setLocale(savedLocale)
-
-        return context.createConfigurationContext(newConfig)
+        val configuration = Configuration(context.resources.configuration)
+        configuration.setLocale(savedLocale)
+        context.resources.updateConfiguration(configuration, context.resources.displayMetrics)
+        return context
     }
 }
