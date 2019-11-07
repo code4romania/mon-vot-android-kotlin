d2dae65ad5b42217b5e784ee4437b76e6cd141e1
Alexandra Daraban <alexandra.daraban@qualitance.com>
2019-11-07 17:59:00 +0200

    updated migrations file

--- app/src/main/java/ro/code4/monitorizarevot/data/Migrations.kt
+++ app/src/main/java/ro/code4/monitorizarevot/data/Migrations.kt
@@ -14,7 +14,7 @@ object Migrations {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.execSQL("ALTER TABLE county ADD COLUMN `diaspora` INTEGER DEFAULT NULL")
             database.execSQL("ALTER TABLE form_details ADD COLUMN `diaspora` INTEGER DEFAULT NULL")
-            database.execSQL("ALTER TABLE form_details ADD COLUMN `order` INTEGER DEFAULT 0")
+            database.execSQL("ALTER TABLE form_details ADD COLUMN `order` INTEGER NOT NULL DEFAULT 0")
         }
     }
 
