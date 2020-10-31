52fe3b7d740b686c26c9c7c226f814a5852deedd
pedrofsn <pedrokra@gmail.com>
2020-10-31 16:31:43 -0300

    Preparing LoginViewModel to read update from remote config

--- app/src/main/java/ro/code4/monitorizarevot/ui/login/LoginViewModel.kt
+++ app/src/main/java/ro/code4/monitorizarevot/ui/login/LoginViewModel.kt
@@ -2,22 +2,30 @@ package ro.code4.monitorizarevot.ui.login
 
 import android.content.SharedPreferences
 import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.OnLifecycleEvent
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
 import org.koin.core.inject
 import ro.code4.monitorizarevot.BuildConfig
 import ro.code4.monitorizarevot.analytics.Event
+import ro.code4.monitorizarevot.data.model.UpdateApp
 import ro.code4.monitorizarevot.data.model.User
 import ro.code4.monitorizarevot.data.model.response.LoginResponse
 import ro.code4.monitorizarevot.helper.*
+import ro.code4.monitorizarevot.helper.Constants.REMOTE_CONFIG_UPDATE_CHECK
+import ro.code4.monitorizarevot.helper.Constants.REMOTE_CONFIG_UPDATE_FORCE
 import ro.code4.monitorizarevot.repositories.Repository
 import ro.code4.monitorizarevot.ui.base.BaseViewModel
 import ro.code4.monitorizarevot.ui.onboarding.OnboardingActivity
 import ro.code4.monitorizarevot.ui.section.PollingStationActivity
+import java.util.*
 
 class LoginViewModel : BaseViewModel() {
 
@@ -26,8 +34,19 @@ class LoginViewModel : BaseViewModel() {
     private val firebaseAnalytics: FirebaseAnalytics by inject()
 
     private val loginLiveData = SingleLiveEvent<Result<Class<*>>>()
+    private val updates = SingleLiveEvent<UpdateApp>()
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun shouldUpdateAppVersion() {
+        FirebaseRemoteConfig.getInstance().apply {
+            val needForceUpdate = getBoolean(REMOTE_CONFIG_UPDATE_FORCE)
+            val needUpdate = getBoolean(REMOTE_CONFIG_UPDATE_CHECK)
+            updates.postValue(UpdateApp(needUpdate, needForceUpdate))
+        }
+    }
 
     fun loggedIn(): LiveData<Result<Class<*>>> = loginLiveData
+    fun needUpdate(): LiveData<UpdateApp> = updates
 
     fun login(phone: String, password: String) {
         loginLiveData.postValue(Result.Loading)
