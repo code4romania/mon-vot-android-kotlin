595b67b8a0bc884ca33f8a7fb9fc00e112e9067c
pedrofsn <pedrokra@gmail.com>
2020-10-31 15:44:25 -0300

    Fix: open and close of progress dialog fragment

--- app/src/main/java/ro/code4/monitorizarevot/ui/guide/GuideFragment.kt
+++ app/src/main/java/ro/code4/monitorizarevot/ui/guide/GuideFragment.kt
@@ -9,31 +9,27 @@ import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_guide.*
 import org.koin.android.ext.android.inject
 import ro.code4.monitorizarevot.R
 import ro.code4.monitorizarevot.ui.base.ViewModelFragment
-import ro.code4.monitorizarevot.widget.ProgressDialogFragment
+
 
 class GuideFragment : ViewModelFragment<GuideViewModel>() {
 
-    override val layout: Int
-        get() = R.layout.fragment_guide
+    override val layout: Int = R.layout.fragment_guide
+    override val screenName: Int = R.string.analytics_title_guide
 
-    override val screenName: Int
-        get() = R.string.analytics_title_guide
-
-    private var progressDialog: ProgressDialogFragment? = null
     override val viewModel: GuideViewModel by inject()
 
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        loadWebView()
+    }
 
-        progressDialog = ProgressDialogFragment().also {
-            it.isCancelable = false
-        }
-
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun loadWebView() {
         webView.apply {
             settings.setSupportZoom(true)
             settings.javaScriptEnabled = true
@@ -41,9 +37,7 @@ class GuideFragment : ViewModelFragment<GuideViewModel>() {
             webViewClient = object : WebViewClient() {
                 override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
                     super.onPageStarted(view, url, favicon)
-                    if (progressDialog?.isResumed == true) {
-                        progressDialog?.showNow(childFragmentManager, ProgressDialogFragment.TAG)
-                    }
+                    openProgressDialog()
                 }
 
                 override fun onPageFinished(view: WebView, url: String) {
@@ -52,24 +46,24 @@ class GuideFragment : ViewModelFragment<GuideViewModel>() {
                         view.reload()
                         return
                     }
-                    progressDialog?.dismiss()
+                    closeProgressDialog()
                 }
 
-                override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-                    return request.url?.let { !it.path.equals(viewModel.url().value) } ?: true
+                override fun shouldOverrideUrlLoading(
+                    view: WebView,
+                    request: WebResourceRequest
+                ): Boolean {
+                    return request.url.path.equals(viewModel.url().value).not()
                 }
             }
         }
+
         viewModel.url().observe(viewLifecycleOwner, Observer {
             webView.loadUrl(it)
         })
     }
 
-    override fun onDestroyView() {
-        if (progressDialog?.isResumed == true) {
-            progressDialog?.dismissAllowingStateLoss()
-            progressDialog = null
-        }
-        super.onDestroyView()
-    }
+    private fun openProgressDialog() = findNavController().navigate(R.id.openProgressDialog)
+    private fun closeProgressDialog() = findNavController().popBackStack()
+
 }
--- app/src/main/java/ro/code4/monitorizarevot/widget/ProgressDialogFragment.kt
+++ app/src/main/java/ro/code4/monitorizarevot/widget/ProgressDialogFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.FragmentManager
 import ro.code4.monitorizarevot.R
 
 /**
@@ -17,6 +16,7 @@ class ProgressDialogFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        isCancelable = false
         return inflater.inflate(R.layout.fragment_progress_dialog, container, false)
     }
 
